12e96818d4fb2752e0823617dd5763dac9c4a833
Stefan Cameron <stefan@stefcameron.com>
2025-01-03 18:46:24 -0600

    Don't let linting be dependent on building

--- tools/node.mjs
+++ tools/node.mjs
@@ -2,6 +2,7 @@
 
 // NOTE: this will load because Node will naturally resolve the external references
 //  to @babel/runtime and lodash in ../node_modules
+// eslint-disable-next-line import/no-unresolved -- this module only exists after a build, and linting shouldn't depend on the build; this is just a manual test file anyway
 import * as rtv from '../dist/node/rtv.slim.mjs';
 global.rtv = rtv;
 
